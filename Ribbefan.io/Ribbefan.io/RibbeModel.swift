32877e13d7f9bbedbfbaf6af6e161255d673a4c6
CPTiPaleAle <149665027+CPTiPaleAle@users.noreply.github.com>
2024-02-19 14:52:22 +0100

    Add files via upload

---
+++ Ribbefan.io/Ribbefan.io/RibbeModel.swift
@@ -0,0 +1,14 @@
+//
+//  RibbeModel.swift
+//  Ribbefan.io
+//
+//  Created by Arin Aksberg-Kristensen on 16/02/2024.
+//
+
+import Foundation
+
+struct RibbeSteg {
+    let tidspunkt: Date
+    let beskrivelse: String
+}
+
